0046b39b40d325ae597ad815c680030b872854b5
Amila <sales@blackcarbonfeast.com>
2019-06-14 11:04:36 +0530

    send java

---
+++ android/app/src/main/java/com/example/my_music_player_plus/models/AudioMedia.java
@@ -0,0 +1,79 @@
+package com.example.my_music_player_plus;
+
+public class AudioMedia {
+    private int id;
+    private String title;
+    private String displayName;
+    private String artist;
+    private long duration;
+    private int track;
+    private String uri;
+    private String type;
+
+    public AudioMedia() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public long getDuration() {
+        return duration;
+    }
+
+    public void setDuration(long duration) {
+        this.duration = duration;
+    }
+
+    public int getTrack() {
+        return track;
+    }
+
+    public void setTrack(int track) {
+        this.track = track;
+    }
+
+    public String getUri() {
+        return uri;
+    }
+
+    public void setUri(String uri) {
+        this.uri = uri;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
